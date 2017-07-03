7dac20b7967bf4ffd0d57f69dc714a9ac134147d
Wes | Atro <wesley.reid@inxabyss.com>
2017-07-03 01:07:22 -0400

    Add files via upload

---
+++ HideKillerName.cs
@@ -0,0 +1,26 @@
+﻿using Oxide.Core;
+using Oxide.Game.Rust;
+using System;
+using System.Collections.Generic;
+
+namespace Oxide.Plugins
+{
+    [Info("HideKillerName", "AtroposOrbis", "1.0.0", ResourceId = 0000)]
+    [Description("Hides name of killers")]
+
+	public class HideKillerName : RustPlugin
+    {
+        #region Initialization
+        StoredData storedData;
+
+        /// <summary>
+        /// New Killer Name
+        /// </summary>
+        string hiddenKillerName = "Hidden";
+
+        /// Permission
+        string perm = "";
+        #endregion
+
+	}
+}
