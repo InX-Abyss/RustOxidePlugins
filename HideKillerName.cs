bff3368d480e5fbd24c6b61944f84dd9a991b040
IzolarePoveste <xavier.greer@inxabyss.com>
2017-07-03 01:22:37 -0400

    Update HideKillerName.cs
    Test_1

--- HideKillerName.cs
+++ HideKillerName.cs
@@ -10,12 +10,37 @@ namespace Oxide.Plugins
 
 	public class HideKillerName : RustPlugin
     {
-        #region Initialization
-        StoredData storedData;
+       // #region Initialization
+       // StoredData storedData;
+       
+	void OnPlayerDie(BasePlayer player, HitInfo info)
+	{
+    		Puts("OnPlayerDie works!");
+		Player = "Anon";
+		
+	/*	class PlayerInfo
+        	{
+           		public string UserId;
+            		public string Name;
 
-        /// <summary>
-        /// New Killer Name
-        /// </summary>
+            		public PlayerInfo()
+            		{
+            		}
+
+            		public PlayerInfo(BasePlayer player)
+            		{
+               			UserId = player.userID.ToString();
+               			Name = player.displayName;
+           		}
+		
+		
+		
+	}
+	*/
+        // <summary>
+        // New Killer Name
+        // </summary>
+	
         string hiddenKillerName = "Hidden";
 
         /// Permission
